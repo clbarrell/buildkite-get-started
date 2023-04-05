b903b9282a9cacc852702472c552c43c69552329
Chris <clbarrell@gmail.com>
2023-04-05 16:15:21 +1000

    fix build

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo -e "--- \033[32m🔨 Build the rocket 🔨\033[0m"
-echo "This is the first step, where we painstakingly put together all 2,303,638 parts of our rocket.
+echo "This is the first step, where we painstakingly put together all 2,303,638 parts of our rocket."
