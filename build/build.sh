ba0384d8c0bbd12c7ca18ef4af330d63e4595dd5
Chris <clbarrell@gmail.com>
2023-04-05 16:42:47 +1000

    foc

--- build/build.sh
+++ build/build.sh
@@ -2,4 +2,4 @@
 
 echo -e "--- \033[32m🔨 Build the rocket 🔨\033[0m"
 echo "This is the first step, where we painstakingly put together all 2,303,638 parts of our rocket."
-echo "This step is being run in parallel, so we can build faster and stronger.
+echo "This step is being run in parallel, so we can build faster and stronger."
