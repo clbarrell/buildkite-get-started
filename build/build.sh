18766e9252cc1cefa7aa3cd93a535f0c54c59558
Chris <clbarrell@gmail.com>
2023-04-27 12:18:21 +1000

    remove parrell and HW, add group

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+PLATFORM=$1
+
+if [ -z "$PLATFORM" ]; then
+  PLATFORM="no platform set"
+fi
+
 echo -e "--- \033[32m🔨 Build the rocket 🔨\033[0m"
 echo "This is the first step, where we painstakingly put together all 2,303,638 parts of our rocket."
-echo "This step is being run in parallel, so we can build faster and stronger."
+echo -e "Building our rocket on: \033[43m$PLATFORM\033[0m"
